f6eaec31af9f710c56368dc49e2cacc41c9d1306
Debraj Banik <writetodebrajbanik@gmail.com>
2024-01-29 01:09:25 +0530

    basic-01

---
+++ App.js
@@ -0,0 +1,13 @@
+let heading = React.createElement("div", { id: "parent" }, [
+  React.createElement("div", { id: "child1" }, [
+    React.createElement("h1", { id: "heading1" }, "Hello from React"),
+    React.createElement("h2", { id: "heading2" }, "Debraj Banik"),
+  ]),
+  React.createElement("div", { id: "child2 " }, [
+    React.createElement("h1", { id: "heading1" }, "Hello from React"),
+    React.createElement("h2", { id: "heading2" }, "Debraj Banik"),
+  ]),
+]);
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
