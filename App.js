29ddccda020e37e692ff87bd6e549ef88773deea
Debraj Banik <writetodebrajbanik@gmail.com>
2024-01-31 02:20:18 +0530

    episode-1

--- App.js
+++ App.js
@@ -1,3 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client'
+
 let heading = React.createElement("div", { id: "parent" }, [
   React.createElement("div", { id: "child1" }, [
     React.createElement("h1", { id: "heading1" }, "Hello from React"),
